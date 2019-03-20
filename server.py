201b8f781664cf1360111b669a1d76b86a1119ea
dror1212 <drortal12@gmail.com>
2019-03-20 15:43:13 +0200

    Change design, Activate random song if no song has been chosen

--- server.py
+++ server.py
@@ -7,7 +7,8 @@ import Tkinter
 import tkFileDialog as fb
 import tkMessageBox
 import os
-
+import random
+import tkFont
 
 class Server():
     def __init__(self):
@@ -15,23 +16,32 @@ class Server():
         self.server.bind(('0.0.0.0',5438))
         self.clients=[]
         self.clientsAdresess = []
+        
         self.new = Thread(target = self.newConnection)
         self.music = Thread(target = self.musicSender)
+        
         self.server.listen(10)
+        
         self.newS = None
+        self.song = None
+        
         self.root = Tkinter.Tk()
         self.root.title("Music Party Controller")
         self.root.protocol("WM_DELETE_WINDOW", self.callback)
         self.root.wm_iconbitmap('head.ico')
+        self.root.config(bg="DarkOrange3")
+        self.root.geometry("800x550")
+        self.root.resizable(0, 0)
         self.file = None
         self.keepGoing = True
+
+        
         
     def main(self):
         self.new.start()
         self.music.start()
-
         self.newSong()
-        
+
     def musicSender(self):
         l = 0
         while self.keepGoing:
@@ -40,66 +50,93 @@ class Server():
                     try:
                         self.file=wave.open(self.newS,'rb')
                         l=self.file.readframes(2048)
+                        self.song = self.newS
+                        self.newS = None
                     except:
                         print ", this song does not exist"
-                self.newS = None
-            if len(self.clients)>0 and l:
-                try:
-                    l=self.file.readframes(2048)
-                    temp = self.clients
-                    counter = 0
-                    for clientSocket in temp:
-                        try:
-                            clientSocket.send(l)
-                        except:
-                            del self.clients[counter]
-                            del self.clientsAdresess[counter]
-                        counter = counter+1
-                except:
-                    pass
+            if len(self.clients)>0:
+                if l and self.file.tell()<=self.file.getnframes():
+                    try:
+                        l=self.file.readframes(2048)
+                        temp = self.clients
+                        counter = 0
+                        for clientSocket in temp:
+                            try:
+                                clientSocket.send(l)
+                            except:
+                                del self.clients[counter]
+                                del self.clientsAdresess[counter]
+                            counter = counter+1
+                    except:
+                        pass
+                else: #play random song
+                    maybeSong = self.song
+                    while self.song==maybeSong:
+                        temp = random.choice(os.listdir(os.path.dirname(os.path.abspath(__file__))))
+                        if ".wav" in temp:
+                            maybeSong=temp
+                    self.newS = maybeSong
+                    
         if self.file!=None:
             self.file.close()
         for clientSocket in self.clients:
             clientSocket.send("ServerSentToClient")
             clientSocket.close()
+            
     def newConnection(self):
         while self.keepGoing:
             (clientSocket,clientAddress)=self.server.accept()
             self.clients.append(clientSocket)
             self.clientsAdresess.append(clientAddress)            
-            print "welcome \n"
+            print "welcome " + str(clientAddress) + "\n"
             
     def chooseNewSong(self):
         self.newS = fb.askopenfilename(title = "Select file",filetypes=[("Wave files", "*.wav")])
 
-    def changeSongTime(self,sec,check,e):
-        try:
-            x = int(sec)
-            if self.file!= None:
-                if check:
+    def changeSongTime(self,check,e):
+        if check==None:
+            self.file.setpos(0)
+        elif self.file!= None and e!=None and e.get()!="":
+            x = int(e.get())
+            if check:
+                if (self.file.getnframes() - self.file.tell())/self.file.getframerate() > x:
                     self.file.setpos(self.file.tell() + self.file.getframerate()*x)
+            else:
+                if self.file.tell()/self.file.getframerate()>x:
+                    self.file.setpos(self.file.tell() - self.file.getframerate()*x)
                 else:
-                    if self.file.tell()/self.file.getframerate()>x:
-                        self.file.setpos(self.file.tell() - self.file.getframerate()*x)
-                    else:
-                        self.file.setpos(0)
-        except:
-            while len(e.get())>0:
-                e.delete(len(e.get())-1)
-                
+                    self.file.setpos(0)
+                    
     def callback(self):
         if tkMessageBox.askokcancel("Quit", "Do you really wish to quit?"):
             self.keepGoing=False;
             self.root.destroy()
             
+    def CheckIfNumber(self, P):
+        if str.isdigit(P) or P == "":
+            return True
+        else:
+            return False
+            
     def newSong(self):
-        changeSong = Tkinter.Button(self.root, text ="Choose song", command = self.chooseNewSong)
+        helv36 = tkFont.Font(family='Helvetica', size=15, weight='bold')
+        helv2 = tkFont.Font(family='Helvetica', size=10, weight='bold')
+        changeSong = Tkinter.Button(self.root, text ="Choose song", command = self.chooseNewSong,bg="light goldenrod",font = helv36)
+        changeSong.config(height=2 , width = 15)
         changeSong.pack()
-        e = Tkinter.Entry(self.root)
+
+        vcmd = (self.root.register(self.CheckIfNumber))
+        
+        large_font = ('Verdana',25)
+        e = Tkinter.Entry(self.root,font=large_font,justify='center', validate='all', validatecommand=(vcmd, '%P'))
+
+        timeChangeF = Tkinter.Button(self.root, text ="Forward", command = lambda: self.changeSongTime(True,e),bg="tomato",font = helv2)
+        timeChangeB = Tkinter.Button(self.root, text ="Backward", command = lambda: self.changeSongTime(False,e),bg="tomato",font = helv2)
+        timeReset = Tkinter.Button(self.root, text ="Restart The Song", command = lambda: self.changeSongTime(None,e),bg="tomato",font = helv2)
+        timeReset.config(height=3, width = 20)
+        timeChangeB.config(height=3, width = 20)
+        timeChangeF.config(height=3, width = 20)
         e.pack()
-        timeChangeF = Tkinter.Button(self.root, text ="Forward", command = lambda: self.changeSongTime(e.get(),True,e))
-        timeChangeB = Tkinter.Button(self.root, text ="Backward", command = lambda: self.changeSongTime(e.get(),False,e))
-        timeReset = Tkinter.Button(self.root, text ="Restart The Song", command = lambda: self.changeSongTime("99999",False,e))
         timeReset.pack()
         timeChangeF.pack()
         timeChangeB.pack()
