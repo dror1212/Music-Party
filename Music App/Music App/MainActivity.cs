23cfcbf6b38df102379fd56ae4fca47d09adf1f8
dror1212 <drortal12@gmail.com>
2019-04-02 12:02:03 +0300

    Need to work on

---
+++ Music App/Music App/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Content;
+
+namespace Music_App
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btn1;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+              
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            btn1 = FindViewById<Button>(Resource.Id.btn1);
+            btn1.Click += Btn1_Click;
+        }
+
+        private void Btn1_Click(object sender, System.EventArgs e)
+        {
+            var intent = new Intent(Player.ActionPlay);
+            StartService(intent);
+        }
+    }
+}
+
---
+++ Music App/Music App/Player.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Music_App
+{
+    [Service]
+    
+    [IntentFilter(new[] { ActionPlay, ActionPause, ActionStop })]
+    class Player : Service
+    {
+        public const string ActionPlay = "com.xamarin.action.PLAY";
+        public const string ActionPause = "com.xamarin.action.PAUSE";
+        public const string ActionStop = "com.xamarin.action.STOP";
+        private MediaPlayer player;
+        public override IBinder OnBind(Intent intent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+        }
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+        [return: GeneratedEnum]
+        public override StartCommandResult OnStartCommand(Intent intent, [GeneratedEnum] StartCommandFlags flags, int startId)
+        {
+            switch (intent.Action)
+            {
+                case ActionPlay: PlayAsync(); break;
+                case ActionStop: Stop(); break;
+                case ActionPause: Pause(); break;
+            }
+            //Set sticky as we are a long running operation
+            return StartCommandResult.Sticky;
+        }
+        private async System.Threading.Tasks.Task PlayAsync()
+        {
+            player = new MediaPlayer();
+            //Tell our player to stream music
+            player.SetAudioStreamType(Stream.Music);
+            //When we have prepared the song start playback
+            player.Prepared += (sender, args) => player.Start();
+            //When we have reached the end of the song stop ourselves, however you could signal next track here.
+            player.Completion += (sender, args) => Stop();
+            player.Error += (sender, args) =>
+            {
+                //playback error
+                Console.WriteLine("Error in playback resetting: " + args.What);
+                Stop();//this will clean up and reset properly.
+
+            };
+            await player.SetDataSourceAsync(ApplicationContext, Android.Net.Uri.Parse("https://www.youtube.com/watch?v=zaf6StO4gy4"));
+            player.PrepareAsync();
+        }
+        private void Stop()
+        {
+
+        }
+        private void Pause()
+        {
+
+        }
+
+    }
+}
