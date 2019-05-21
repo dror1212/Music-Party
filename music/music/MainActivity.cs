a274d32fcb6772cde717e9deb2111c2d044ff608
dror1212 <drortal12@gmail.com>
2019-05-21 23:08:12 +0300

    Need to check

--- music/music/MainActivity.cs
+++ music/music/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.OS;
 using Android.Support.V7.App;
 using System.Net.Sockets;
 using System.Threading;
+using Android.Media;
 using System;
 using System.Text;
 
@@ -16,7 +17,7 @@ namespace music
         EditText edt1;
         TcpClient client;
         NetworkStream stream;
-        private byte[] datalength;
+        AudioTrack player;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -25,13 +26,13 @@ namespace music
             SetContentView(Resource.Layout.activity_main);
             btnSend = FindViewById<Button>(Resource.Id.btnSend);
             edt1 = FindViewById<EditText>(Resource.Id.edt1);
+            player = new AudioTrack(global::Android.Media.Stream.Music, 88000, ChannelOut.Mono, Android.Media.Encoding.Pcm16bit,32,AudioTrackMode.Stream);
             btnSend.Click += BtnSend_Click;
-            datalength = new byte[32];
         }
 
         private void BtnSend_Click(object sender, System.EventArgs e)
         {
-            client = new TcpClient("10.30.56.204", 3334);
+            client = new TcpClient("192.168.1.105", 3334);
             clientReceive();
         }
 
@@ -40,13 +41,17 @@ namespace music
             try
             {
                 stream = client.GetStream(); //Gets The Stream of The Connection
+                //byte[] x = System.Text.Encoding.ASCII.GetBytes("Connection:d,1");
+                //stream.Write(x, 0, x.Length);
+                player.Play();
                 new Thread(() => // Thread (like Timer)
                 {
                     while (true)//Keeps Trying to Receive the Size of the Message or Data
                     {
+                        byte[] datalength = new byte[32];
                         if ((stream.Read(datalength, 0, 32)) != 0)
                         {
-                            this.RunOnUiThread(() => this.edt1.Text = this.edt1.Text + "kkk");
+                            this.RunOnUiThread(() => PlaySound(datalength));
                         }
                     }
                 }).Start(); // Start the Thread
@@ -55,6 +60,11 @@ namespace music
             {
                 Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
             }
+        }
+
+        private void PlaySound(byte[] data)
+        {
+            player.Write(data, 0, 32);
         }
     }
 }
