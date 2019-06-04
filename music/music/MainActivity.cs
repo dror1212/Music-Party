b0b4f39f3036959f6cacde0adbb70692be5138ad
dror1212 <drortal12@gmail.com>
2019-06-04 11:43:06 +0300

    Need to fix

--- music/music/MainActivity.cs
+++ music/music/MainActivity.cs
@@ -29,40 +29,11 @@ namespace music
             btnSend1 = FindViewById<Button>(Resource.Id.btnSend1);
             edt1 = FindViewById<EditText>(Resource.Id.edt1);
             btnSend.Click += BtnSend_Click;
-            btnSend1.Click += BtnSend1_Click;
-        }
-
-        private void BtnSend1_Click(object sender, EventArgs e)
-        {
-            var bytes = default(byte[]);
-            using (var streamReader = new StreamReader("C:\\Users\\student\\Desktop\\Music-Party-check\\songs\\Counting Stars.wav"))
-            {               
-                using (var memstream = new MemoryStream())
-                {
-                    streamReader.BaseStream.CopyTo(memstream);
-                    bytes = memstream.ToArray();
-                }
-            }
-            int max = 33;
-            int count = 0;
-            while(true)
-            {
-                byte[] b = new byte[32];
-                int count2 = count;
-                while (count2 < max)
-                {
-                    b[count] = bytes[count];
-                    count2 = count2+1;
-                }
-                count = max;
-                max = max + max - 1;
-                PlaySound(bytes);
-            };
         }
 
         private void BtnSend_Click(object sender, System.EventArgs e)
         {
-            client = new TcpClient("10.30.57.145", 3539);
+            client = new TcpClient("10.30.56.227", 3539);
             clientReceive();
         }
 
@@ -73,17 +44,17 @@ namespace music
                 stream = client.GetStream(); //Gets The Stream of The Connection
                 //byte[] x = System.Text.Encoding.ASCII.GetBytes("Connection:d,1");
                 //stream.Write(x, 0, x.Length);
-                new System.Threading.Thread(() => // Thread (like Timer)
+                //new System.Threading.Thread(() => // Thread (like Timer)
+                //{
+                while (true)//Keeps Trying to Receive the Size of the Message or Data
                 {
-                    while (true)//Keeps Trying to Receive the Size of the Message or Data
+                    byte[] datalength = new byte[131072];
+                    if ((stream.Read(datalength, 0, 131072)) != 0)
                     {
-                        byte[] datalength = new byte[32];
-                        if ((stream.Read(datalength, 0, 32)) != 0)
-                        {
-                            PlaySound(datalength);
-                        }
+                        PlaySound(datalength);
                     }
-                }).Start(); // Start the Thread
+                }
+                //}).Start(); // Start the Thread
             }
             catch (System.Exception ex)
             {
@@ -95,14 +66,14 @@ namespace music
         {
             try
             {
-#pragma warning disable CS0618 // Type or member is obsolete
-                AudioTrack player = new AudioTrack(Android.Media.Stream.Music, 8000, ChannelOut.Mono, Android.Media.Encoding.Pcm16bit, 32, AudioTrackMode.Static);
-#pragma warning restore CS0618 // Type or member is obsolete
-                player.Write(data, 0, 32);
+                AudioTrack player = new AudioTrack(Android.Media.Stream.Music, 88800, ChannelOut.Mono, Android.Media.Encoding.Pcm16bit, 131072, AudioTrackMode.Static);
+                player.Write(data, 0, 131072);
                 player.Play();
             }
-            catch(IllegalStateException)
-            {}
+            catch(IllegalStateException ex)
+            {
+                Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+            }
         }
     }
 }
