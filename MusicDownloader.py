7c9c75e9ee3b2ea0aeb1e2d88da551e7fc4c5864
dror1212 <drortal12@gmail.com>
2019-06-15 21:01:42 +0300

    Name only in hebrew

--- MusicDownloader.py
+++ MusicDownloader.py
@@ -6,15 +6,24 @@ import os
 
 class musicDownloader():
     def __init__(self):
+        self.allowedChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','-','.','_',',','\\','/','*']
         pass
 
     def downloadSong(self,url,title):
+        if "www.youtube.com/watch?v=" not in url:
+            return "something is wrong with the url"
         temp = os.listdir(os.getcwd()+"\\songs")
+
+        for char in title:
+            if char not in self.allowedChars:
+                return "You can name the songs only in english"
+        name = 'songs\\' + title
+
         for name in temp:
             if str(name).split(".")[0]==title:
                 return "This song is already exist"
         print temp
-        name = 'songs\\' + title
+        
         try:
             ydl_opts = {
                 'format': 'bestaudio/best',
@@ -32,4 +41,4 @@ class musicDownloader():
                 #os.rename('songs\\aaaa.wav', name)
             return "download completed"
         except:
-            return "something is wrong with the url"
+            return "something is wrong"
