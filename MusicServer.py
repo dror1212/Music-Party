376f8a7bcbb7c972452353b362cce274bd7a4d89
dror1212 <drortal12@gmail.com>
2019-06-18 21:53:18 +0300

    seperate server gui from server data

---
+++ MusicServer.py
@@ -0,0 +1,125 @@
+import socket
+import pyaudio
+import wave
+from time import sleep
+from time import time
+from threading import Thread
+import Tkinter
+import tkFileDialog as fb
+import tkMessageBox
+import os
+import random
+import tkFont
+import pickle
+import md5
+from connection import SocketManager
+from DataBase import DataBase
+from MusicDownloader import musicDownloader
+from recorder import Recorder
+
+class MusicServer(object):
+    def __init__(self):
+        #creating my socket to connect with others
+        self.server = SocketManager(3540)
+        
+        #creating the base for the gui
+        self.root = Tkinter.Tk()
+        self.data = DataBase("DataBase.txt",self.root)
+        self.currentSong = -1 ####################
+        self.newS = None #saving the location for new songs
+        self.song = None #the song that currently is running  
+        self.file = None #the file of the song
+        self.keepGoing = True #controll the running system
+        self.stop = False #stop or play the song
+        self.rand = True
+
+    def musicSender(self):
+        l = 0 #false
+        while self.keepGoing: #if the gui is still open
+            if True in self.server.clients.values() and self.file!=None: #if there is someone connected
+                if self.file.tell()<=self.file.getnframes(): #if thre is info and the song is not over
+                        if not self.stop: #if the song is not on stop mode
+                            l=self.file.readframes(16234) #read from the song file
+                            self.server.broadcast(l)
+                            sleep(0.363) 
+
+        if self.file!=None:
+            self.file.close()
+        for clientSocket in self.server.clients: #disconnect from all the clients
+            clientSocket.close()
+
+    def musicChanger(self):
+        while self.keepGoing: #if the gui is still open
+            if None!=self.newS: #if there is new song being asked
+                if self.newS!="": #to check it is not empty string
+                    try:
+                        if self.file!=None: #if its not the first song being played
+                            self.file.close() #close the last song
+                        self.file=wave.open(self.newS,'rb') #open the new song
+
+                        #save the current song with the right syntax
+                        temp = self.newS.split("\\")                        
+                        temp = temp[len(temp)-1] 
+                        temp = temp.split("/")
+                        self.song = temp[len(temp)-1]
+                        
+                        self.newS = None
+                    except:
+                        print self.newS
+                        #print "sorry, there is a problem"
+            if True in self.server.clients.values():
+                if self.file!=None: #if there is someone connected
+                    if not self.stop and len(self.server.clients)>0 and self.file.tell()==self.file.getnframes(): #play random song if no song has been chosen
+                        if self.rand:
+                            self.chooseRandomSong() #activate random song if no song is being played
+                        else:
+                            self.chooseNextSong()
+                else:
+                    self.chooseNextSong()
+
+    def chooseRandomSong(self):
+            maybeSong = self.song #make sure the random song is not the last one
+            while self.song==maybeSong: #make sure the random song is not the last one
+                temp = random.choice(os.listdir(os.getcwd()+"\\songs")) #choose random file from the songs
+                self.currentSong = os.listdir(os.getcwd()+"\\songs").index(temp)
+                print temp
+                print self.song
+                if ".wav" in temp:
+                    maybeSong=temp
+            self.newS = "songs\\"+maybeSong #save the path to the song
+            
+    def chooseNextSong(self):
+        self.otherSong(True)
+        
+    def otherSong(self,mode):
+        if True in self.server.clients.values(): #if there is someone connected
+            temp = os.listdir(os.getcwd()+"\\songs")
+            if len(temp)>0:
+                if mode:
+                    if len(temp)>self.currentSong+1:
+                        self.currentSong+=1
+                        s = temp[self.currentSong]
+                    else:
+                        s = temp[0]
+                        self.currentSong=0
+                else:
+                    if self.currentSong != 0:
+                        self.currentSong-=1
+                        s = temp[self.currentSong]
+                    else:
+                        s = temp[-1]
+                        self.currentSong=len(temp)-1
+                print s     
+                if ".wav" in s:
+                    self.newS = "songs\\"+s #save the path to the song
+                    
+    def newConnection(self): #while the app is working wait for new clients to join and add them
+        while self.keepGoing:
+            (clientSocket,clientAddress)=self.server.accept()
+            self.server.clients[clientSocket] = False
+            self.server.clientsAdresess.append(clientAddress)
+            self.server.names[clientSocket]= None
+            self.listen = Thread(target = self.server.listen_to_clients,
+                                 args = (clientSocket,self.data,))
+            self.listen.start()
+            print "welcome " + str(clientAddress) + "\n"
